27e5d9b33e95e1d7334c3bd87f6e9a070d724db0
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-06-06 05:32:01 +0530

    app

--- backend/resources/js/echo.js
+++ backend/resources/js/echo.js
@@ -7,7 +7,7 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST, 
-    wsPath: '/app', // ✅ this is what adds `/app` in the actual WebSocket URL
+    wsPath: '', // 
     wsPort: 80,
     wssPort: 443,
     forceTLS: true,
