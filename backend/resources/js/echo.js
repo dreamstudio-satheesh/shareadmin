d9234930224be439b531cdba1b7532e0aec78b00
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-06-06 05:45:02 +0530

    app

--- backend/resources/js/echo.js
+++ backend/resources/js/echo.js
@@ -6,10 +6,9 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST, 
-    wsPath: '', // 
+    wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: 80,
-    wssPort: 443,
-    forceTLS: true,
-    enabledTransports: ['ws', 'wss'],
+   // wssPort: 443,
+    forceTLS: false,
+    enabledTransports: ['ws'],
 });
