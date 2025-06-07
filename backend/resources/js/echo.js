26acc598be51b616035474352bbf67d1a83a6da3
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-06-07 15:48:52 +0530

    app

--- backend/resources/js/echo.js
+++ backend/resources/js/echo.js
@@ -1,16 +1,27 @@
 import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
 
+import Pusher from 'pusher-js';
 window.Pusher = Pusher;
+
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: parseInt(import.meta.env.VITE_REVERB_PORT || '8080'),
-    wssPort: parseInt(import.meta.env.VITE_REVERB_PORT || '443'),
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME || 'https') === 'https',
-    enabledTransports: ['ws'], // keep only 'ws' since you're using http
+    key: 'pnxogxkyluuvpe7i4mhn',      
+    wsHost: '138.199.213.241',        
+    wsPort: 8080,
+    wssPort: 8080,
+    forceTLS: false,                  
+    enabledTransports: ['ws'],
 });
 
+
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws'],
+// });
 // enabledTransports: ['ws', 'wss'],
