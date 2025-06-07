1283c2628b5645ff4bb52a21d43f74b17b1bbc90
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-06-07 15:51:32 +0530

    app

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,5 +1,18 @@
-import './bootstrap';
+// import './bootstrap';
 
-// app imports
+import Echo from 'laravel-echo';
 
-// Vue imports
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
+
+
+window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: 'pnxogxkyluuvpe7i4mhn',      
+    wsHost: '138.199.213.241',        
+    wsPort: 8080,
+    wssPort: 8080,
+    forceTLS: false,                  
+    enabledTransports: ['ws'],
+});
+
